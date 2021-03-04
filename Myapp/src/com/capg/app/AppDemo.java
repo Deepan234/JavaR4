d17d409dd82c586f5c57a46e5ba60686eb189c9f
Deepan234 <80026698+Deepan234@users.noreply.github.com>
2021-03-04 13:15:45 +0530

    new update from deepan

--- Myapp/src/com/capg/app/AppDemo.java
+++ Myapp/src/com/capg/app/AppDemo.java
@@ -5,5 +5,7 @@ public class AppDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
+		System.out.println("Hello Deepan");
+		System.out.println("Hello pranav");
 	}
 }
